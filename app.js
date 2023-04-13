2534422caf87b66b0472e8befd3ae3be54887dcb
Maltemork <maltems1996@gmail.com>
2023-04-13 15:12:25 +0200

    løst en masse

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const names = [
     "Birgitte"
 ];
 
+
+
 const years = [2003, 2032, 1990, 1966, 1998, 1989, 1975, 2023, 1933];
 
 const teachers = [
@@ -80,9 +82,120 @@ const teacher = {
 // ========== initApp ========== //
 
 function initApp() {
+    names.forEach(loopThroughNames);
     console.log("initApp: app.js is running 🎉");
+    loopThroughFiveFirstYears();
+    logNumbers();
+    teachersOver40();
+    forOfSearchTeachersByName("Pet");
+    forInTeacher();
+    whileTeachersIsLecturer();
+    whileSearchByName("sen");
 }
 
+
+function loopThroughNames(name, index){
+    if (index === 0) {
+        console.log("Dette er første element");
+    }
+    if (index === names.length-1) {
+        console.log("Dette er sidste element");
+    }
+    if (index % 2 === 1) {
+        console.log("Hver anden.");
+    }
+
+
+    console.log(name);
+    console.log(index);
+}
+
+
+function sommomo() {
+    for (let index = 0; index < names.length; index++) {
+		const name = names[index];
+		console.log(name);
+}
+}
+
+function loopThroughYears() {
+    for (let index = 0; index < years.length; index++) {
+        console.log( years[index]);
+        if (years[index] === 2023){
+            console.log("This is the current year");
+        }
+    }
+}
+
+function loopThroughFiveFirstYears() {
+    for (let index = 0; index < 5; index++) {
+        console.log( years[index]);
+    }
+}
+
+function loopThroughFiveLastYears() {
+    for (let index = years.length-5; index < years.length; index++) {
+        console.log( years[index]);
+}
+}
+
+function logNumbers() {
+    for (let i = 100; i >= 0; i-=10) {
+        console.log(i);
+    }
+}
+
+function loopThroughTeachers() {
+    for (const t of teachers) {
+        console.log(t);
+    }
+}
+
+function teachersOver40() {
+    const results = [];
+    for (const t of teachers) {
+        if (t.age >= 40) {
+            results.push(t);
+            console.log(results);
+        }
+    }
+}
+
+function forOfSearchTeachersByName(searchValue) {
+    const results = [];
+    for (const n of teachers) {
+        if (n.name.includes(searchValue)) {
+            results.push(n);
+            console.log(results);
+        }
+    }
+}
+
+function forInTeacher() {
+    for (const key in teacher) {
+        console.log(teacher[key]);
+    }
+}
+
+function whileTeachersLength() {
+    let index = 0;
+    while (index < teachers.length) {
+        const name = teachers[index];
+        console.log(name);
+        index++;
+    }
+}
+
+function whileTeachersIsLecturer() {
+    let index = 0;
+    while (index < teachers.length) {
+        const name = teachers[index];
+        if (name.title == "Lecturer") {
+        console.log(name);
+        }
+        index++;
+    }
+}
 // ========== forEach ========== //
 
 // ========== for loop ========== //
@@ -92,6 +205,24 @@ function initApp() {
 // ========== for in loop ========== //
 
 // ========== while loops ========== //
+
+function whileSearchByName(searchValue) {
+    searchValue = searchValue.toLowerCase();
+    const results = [];
+    
+    console.log(searchValue);
+    let index = 0;
+
+    while (index < teachers.length) {
+        const teacher = teachers[index];
+        const name = teacher.name.toLowerCase();
+        if (teacher.name.includes(searchValue)) {
+            results.push(teacher);
+        }
+    index++;
+    console.log(results);
+    }
+}
 
 // ========== Filter items: array.filter(...) ========== //
 
